c9669bbcee4eb0f12cc445925385fae266fedcff
MH.Ji <battlesun99@gmail.com>
2021-07-22 15:37:56 +0900

    Feat: post register api and create response data

--- app.js
+++ app.js
@@ -23,6 +23,20 @@ const port = 8080
 // middleware function
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 
+app.post('/restapi/robot/v1/regiter', (req, res) => {
+    // 위의 URI로 데이터 보내주기
+    let register_json = fs.readFileSync(path.join(appRoot, './routes/json/register_success.json'))
+    let register_data = base64encode(JSON.stringify(JSON.parse(register_json)))
+
+    console.log(register_data)
+    res.setHeader('Content-Type', 'application/json;charset=utf-8')
+    res.send(register_data)
+})
+
+app.post('/restapi/robot/v1/report', (req, res) => {
+    console.log("report request body = ", base64decode(JSON.stringify(req.body)))
+})
+
 app.listen(port, () => {
     console.log("Start Rest Api Test Server Listening...")
 })
