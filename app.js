9237fa8fcc6e3a4e28264a8d000ec466c2cd8043
MH.Ji <battlesun99@gmail.com>
2021-07-22 17:08:27 +0900

    Feat: revise report api

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { base64encode, base64decode } = require('nodejs-base64')
 
 const swaggerUI = require('swagger-ui-express')
 const swaggerJSDoc = require('swagger-jsdoc')
-const { report } = require('process')
+const { response } = require('express')
 const swaggerSpec = swaggerJSDoc({
     swaggerDefinition: yaml.load(path.join(appRoot, 'routes/yaml/swagger_api.yaml')),
     apis: [
@@ -19,6 +19,11 @@ const swaggerSpec = swaggerJSDoc({
 const app = express()
 const port = 5000
 
+let robot_id = "SeRo01"
+let terminal_id = "P03"
+let zone_id = "1"
+let service_mode = "2"
+
 // middleware function
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 
@@ -27,6 +32,13 @@ app.listen(port, () => {
 })
 
 app.post('/restapi/robot/v1/regiter', (req, res) => {
+    let request_register_json = JSON.parse(JSON.stringify(req.body))
+
+    robot_id = request_register_json.register.id
+    terminal_id = request_register_json.register.terminal
+    zone_id = request_register_json.register.zone
+    service_mode = request_register_json.register.sort
+
     // 위의 URI로 데이터 보내주기
     let register_json = fs.readFileSync(path.join(appRoot, 'routes/json/register_success.json'))
     let register_data = base64encode(JSON.stringify(JSON.parse(register_json)))
@@ -36,9 +48,38 @@ app.post('/restapi/robot/v1/regiter', (req, res) => {
 })
 
 app.post('/restapi/robot/v1/report', (req, res) => {
-    let report_json = fs.readFileSync(path.join(appRoot, 'routes/json/report_success.json'))
-    let report_data = base64encode(JSON.stringify(JSON.parse(report_json)))
+    let today = new Date();
+    let tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1)
+
+    let format_today = `${today.getFullYear().toString()}/${today.getMonth().toString().padStart(2, "0")}/${today.getDate().toString().padStart(2, "0")}`
+    let format_tomorrow = tomorrow.getFullYear().toString() + "/" + tomorrow.getMonth().toString().padStart(2, "0") + "/" + tomorrow.getDate().toString().padStart(2, "0")
+
+    let response_report_json = JSON.parse(fs.readFileSync(path.join(appRoot, 'routes/json/report_success.json')))
+    response_report_json.cmd.forEach((item) => {
+        [start_ymd, start_time] = item.starttime.split(" ")
+        [start_y, start_m, start_d] = start_ymd.split("/")
+        [end_ymd, end_time] = item.endtime.split(" ")
+
+        // starttime 연도가 1900으로 설정되어 있다면, 오늘 날짜로 변경해주기
+        if (start_y === "1900") {
+            item.starttime = `${format_today} ${start_time}`
+            item.endtime = `${format_today} ${end_timd}`
+
+            // endtime이 starttime보다 작다면 endtime을 내일 날짜로 변경해주기
+            // javascript에서는 두 날짜를 비교연산자를 통해 즉시 비교 가능함.
+            if (start_time > end_time) {
+                item.endtime = `${format_tomorrow} ${end_time}`
+            }
+        }
+
+        item.mode = service_mode
+    })
+
+    response_report_json.id = robot_id
+    response_report_json.time = `${today.getFullYear().toString()}/${today.getMonth().toString().padStart(2, "0")}/${today.getDate().toString().padStart(2, "0")} ${today.getHours().toString().padStart(2, "0")}:${today.getMinutes().toString().padStart(2, "0")}:${today.getSeconds().toString().padStart(2, "0")}`
+    response_report_json = JSON.stringify(response_report_json)
 
     res.setHeader('Content-Type', 'application/json;charset=utf-8')
-    res.send(report_data)
+    res.send(response_report_json)
 })
